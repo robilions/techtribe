7267b9ced8c9a5d16e312ca7df80ede708f28e41
wisnu112233 <wisnuafif7@gmail.com>
2025-01-09 23:33:40 +0700

    Fix Transaksi

---
+++ database/migrations/2025_01_09_162654_add_metode_bayar_to_penjualan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMetodeBayarToPenjualanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('penjualan', function (Blueprint $table) {
+            $table->string('metode_bayar')->nullable(); // Adding the 'metode_bayar' column (nullable)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('penjualan', function (Blueprint $table) {
+            $table->dropColumn('metode_bayar'); // Drop the 'metode_bayar' column
+        });
+    }
+}
