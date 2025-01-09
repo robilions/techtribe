155f5f8a1b2824ce7b8c6061ffe4af9216382151
robilions <130848931+robilions@users.noreply.github.com>
2025-01-09 22:35:34 +0700

    hapus supplier dan member

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,70 +12,39 @@
 @section('content')
     <!-- Small boxes (Stat box) -->
     <div class="row">
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3>{{ $kategori }}</h3>
-
-                    <p>Total Kategori</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-cube"></i>
-                </div>
-                <a href="{{ route('kategori.index') }}" class="small-box-footer">Lihat <i
-                        class="fa fa-arrow-circle-right"></i></a>
+    <div class="col-lg-6 col-xs-12">
+        <!-- small box -->
+        <div class="small-box bg-aqua">
+            <div class="inner">
+                <h3>{{ $kategori }}</h3>
+                <p>Total Kategori</p>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-green">
-                <div class="inner">
-                    <h3>{{ $produk }}</h3>
-
-                    <p>Total Produk</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-cubes"></i>
-                </div>
-                <a href="{{ route('produk.index') }}" class="small-box-footer">Lihat <i
-                        class="fa fa-arrow-circle-right"></i></a>
+            <div class="icon">
+                <i class="fa fa-cube"></i>
             </div>
+            <a href="{{ route('kategori.index') }}" class="small-box-footer">
+                Lihat <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-yellow">
-                <div class="inner">
-                    <h3>{{ $member }}</h3>
-
-                    <p>Total Member</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-id-card"></i>
-                </div>
-                <a href="{{ route('member.index') }}" class="small-box-footer">Lihat <i
-                        class="fa fa-arrow-circle-right"></i></a>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-6 col-xs-12">
+        <!-- small box -->
+        <div class="small-box bg-green">
+            <div class="inner">
+                <h3>{{ $produk }}</h3>
+                <p>Total Produk</p>
             </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-red">
-                <div class="inner">
-                    <h3>{{ $supplier }}</h3>
-
-                    <p>Total Supplier</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-truck"></i>
-                </div>
-                <a href="{{ route('supplier.index') }}" class="small-box-footer">Lihat <i
-                        class="fa fa-arrow-circle-right"></i></a>
+            <div class="icon">
+                <i class="fa fa-cubes"></i>
             </div>
+            <a href="{{ route('produk.index') }}" class="small-box-footer">
+                Lihat <i class="fa fa-arrow-circle-right"></i>
+            </a>
         </div>
-        <!-- ./col -->
+    </div>
+</div>
+
     </div>
     <!-- /.row -->
     <!-- Main row -->
